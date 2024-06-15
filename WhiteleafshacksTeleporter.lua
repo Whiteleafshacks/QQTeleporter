ad4328cd1d3fed5f091e7848ab14db52340ad86c
Whiteleafshacks <166086648+Whiteleafshacks@users.noreply.github.com>
2024-06-15 08:55:57 +0700

    Create WhiteleafshacksTeleporter.lua

---
+++ WhiteleafshacksTeleporter.lua
@@ -0,0 +1,18 @@
+game.StarterGui:SetCore("SendNotification",
+  {
+    Title = "Whiteleafshacks Teleporter Notice";
+    Text = "Made By Whiteleafshacks";
+    Icon = "";
+    Duration = 20;
+  })
+
+mouse = game.Players.LocalPlayer:GetMouse()
+tool = Instance.new("Tool")
+tool.RequiresHandle = false
+tool.Name = "Whiteleafshacks Teleport"
+tool.Activated:connect(function()
+local pos = mouse.Hit+Vector3.new(0,2.5,0)
+pos = CFrame.new(pos.X,pos.Y,pos.Z)
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = pos
+end)
+tool.Parent = game.Players.LocalPlayer.Backpack
